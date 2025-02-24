fe9290fe5fffd423c1cbac22c82506d3919e17d8
claycot <claycotton@hotmail.com>
2025-02-23 22:09:02 -0500

    [claycot] Set size for buttons to show full character.

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,8 @@
 package src;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -53,6 +55,8 @@ public class Game {
                         }
                     }
                 });
+                button.setPreferredSize(new Dimension(50, 50));
+                button.setFont(new Font("Arial", Font.PLAIN, 20));
                 panel.add(button);
             }
         }
